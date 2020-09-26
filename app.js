3b003cb98486c1473b727bab44c6172e38761244
Horace Liu <horaceliu.hl@gmail.com>
2020-09-26 11:05:32 -0400

    COVID19 Dashboard V3

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ async function getLat() {
 
   console.log(d.data[0].name + "FFFFFF");
   var tempasdf = Object.keys(tempHashTable);
-  console.log(tempasdf);
+  console.log(tempHashTable.keys);
 
   for (let i = 0; i < d.data.length; i++) {
     confirmedCases = d.data[i].latest_data.confirmed;
